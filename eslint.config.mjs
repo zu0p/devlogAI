5f67780edd5c633dece03e97d6a10e3076b35f24
zu0p <zu0p@kakao.com>
2026-02-04 12:15:43 +0900

    chore: lint 규칙 추가 - client 컴포넌트에서 server 전용 api 사용 금지 - 잘못된 hook 사용 금지 - useEffect async 직접 사용 워닝 - hook dependency 누락 워닝 - any type 워닝 - console log 워닝

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,61 @@
-import { defineConfig, globalIgnores } from "eslint/config"
-import nextVitals from "eslint-config-next/core-web-vitals"
-import nextTs from "eslint-config-next/typescript"
-import unusedImports from "eslint-plugin-unused-imports"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+import unusedImports from "eslint-plugin-unused-imports"
+import storybook from "eslint-plugin-storybook"
+import reactHooks from "eslint-plugin-react-hooks"
+import tseslint from "@typescript-eslint/eslint-plugin"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const eslintConfig = [
   {
     plugins: {
       "unused-imports": unusedImports,
+      "react-hooks": reactHooks,
+      "@typescript-eslint": tseslint,
     },
     rules: {
       "unused-imports/no-unused-imports": "warn",
       "unused-imports/no-unused-vars": [
         "warn",
         {
-          vars: "all",
           varsIgnorePattern: "^_",
-          args: "after-used",
           argsIgnorePattern: "^_",
+        },
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error"],
         },
       ],
     },
   },
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+  }),
 
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-])
+  ...storybook.configs["flat/recommended"],
+]
 
 export default eslintConfig
