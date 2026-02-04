1eb1186b4e1fe2e906585324f6270d13102571ed
zu0p <zu0p@kakao.com>
2026-02-04 14:15:03 +0900

    fix: lint 충돌 에러 -> 린트 규칙 최소화

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,7 @@
 import { dirname } from "path"
 import { fileURLToPath } from "url"
 import { FlatCompat } from "@eslint/eslintrc"
-
 import unusedImports from "eslint-plugin-unused-imports"
-import reactHooks from "eslint-plugin-react-hooks"
-import tseslint from "@typescript-eslint/eslint-plugin"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -17,31 +14,9 @@ const eslintConfig = [
   {
     plugins: {
       "unused-imports": unusedImports,
-      "react-hooks": reactHooks,
-      "@typescript-eslint": tseslint,
     },
     rules: {
       "unused-imports/no-unused-imports": "warn",
-      "unused-imports/no-unused-vars": [
-        "warn",
-        {
-          varsIgnorePattern: "^_",
-          argsIgnorePattern: "^_",
-        },
-      ],
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        {
-          checksVoidReturn: {
-            attributes: false,
-          },
-        },
-      ],
-
-      "@typescript-eslint/no-explicit-any": "warn",
-
       "no-console": [
         "warn",
         {
