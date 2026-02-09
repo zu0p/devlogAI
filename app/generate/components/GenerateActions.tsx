96c7fa7096b3a5363faba0323907377b58293a9a
zu0p <zu0p@kakao.com>
2026-02-09 10:23:14 +0900

    refactor: 필요없는 console 제거

--- app/generate/components/GenerateActions.tsx
+++ app/generate/components/GenerateActions.tsx
@@ -1,14 +1,9 @@
 import { GenerateActionsProps } from "../types"
 
 const GenerateActions = ({ onMutation }: GenerateActionsProps) => {
-  const handleGenerate = async () => {
-    const article = await onMutation()
-    console.log(article)
-  }
-
   return (
     <>
-      <button onClick={handleGenerate}>생성하기</button>
+      <button onClick={onMutation}>생성하기</button>
     </>
   )
 }
