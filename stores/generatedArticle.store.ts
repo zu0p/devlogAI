a64275370f87bb411908ffe5edc9d4d8e0372769
zu0p <zu0p@kakao.com>
2026-02-27 02:11:41 +0900

    feat: api response에서 hashtags, meta 정보 저장 - 전역상태로 저장(zustand)에 상태추가 - api call 했을 때 저장

--- stores/generatedArticle.store.ts
+++ stores/generatedArticle.store.ts
@@ -2,18 +2,26 @@ import { create } from "zustand"
 
 type GeneratedArticleState = {
   content: string | null
+  hashtags: string[] | null
+  metaDescription: string | null
   isLoading: boolean
   error: Error | null
   setGeneratedArticle: (content: string | null) => void
+  setGeneratedHashtags: (hashtags: string[] | null) => void
+  setGeneratedMetaDescription: (metaDescription: string | null) => void
   setLoading: (loading: boolean) => void
   setError: (error: Error | null) => void
 }
 
 export const useGeneratedArticle = create<GeneratedArticleState>((set) => ({
   content: null,
+  hashtags: null,
+  metaDescription: null,
   isLoading: false,
   error: null,
   setGeneratedArticle: (content) => set({ content }),
+  setGeneratedHashtags: (hashtags) => set({ hashtags }),
+  setGeneratedMetaDescription: (metaDescription) => set({ metaDescription }),
   setLoading: (loading) => set({ isLoading: loading }),
   setError: (error) => set({ error }),
 }))
